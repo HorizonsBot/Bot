74ae88756f8ddf7d7cccb04dc0d25eb890de8a67
Andrew Eells <andrew.eells3@gmail.com>
2017-07-19 11:04:52 -0700

    updating conflicts with merge that didn't get dealt with in models

--- models.js
+++ models.js
@@ -5,12 +5,9 @@ var userSchema = mongoose.Schema({
     access_token: String,
     refresh_token: String,
     profile_ID: String,
-<<<<<<< HEAD
     expiry_date: Number,
     profile_name: String
-=======
-    expiry_date: Number
->>>>>>> master
+
   },
   slack_ID: String,
   slack_Username: String,
@@ -19,31 +16,18 @@ var userSchema = mongoose.Schema({
 });
 
 var reminderSchema = mongoose.Schema({
-<<<<<<< HEAD
-=======
-  // slack_DM_ID: String,
->>>>>>> master
   subject: {
     required: true,
     type: String
   },
   day: {
     required: true,
-<<<<<<< HEAD
-    type: Date
-=======
     type: String
->>>>>>> master
   },
   googCalID: String,
   reqID: String
 })
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 var User = mongoose.model('User', userSchema);
 var Reminder = mongoose.model('Reminder', reminderSchema);
 
