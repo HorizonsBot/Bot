9d3cbff0a713b79075fadb56a3b7e450c4ea46af
yashnevatia <yash1997@hku.hk>
2017-07-20 11:23:54 -0700

    here

--- app.js
+++ app.js
@@ -58,6 +58,161 @@ var obj = {
   ]
 }
 
+// checking conflict
+
+var getWeekArray = function(date, time){
+
+  console.log("entered getWeekArray");
+  var startString = date + time;
+  var a = moment(date);
+  var b = a.add(7, 'day'); //make this shit a moment god dammit
+  var c = b.format().substring(0,19);
+  var endString = c.split('T').join(' ');
+  var start = moment(startString, 'YYYY-MM-DD hh:mm a');
+  var end = moment(endString, 'YYYY-MM-DD hh:mm a');
+  var result = [];
+  var current = moment(start);
+  while (current <= end) {
+        result.push(current.format('YYYY-MM-DD HH:mm'));
+        current.add(30, 'minutes');
+  }
+
+  console.log("original weekArray", result);
+
+  return result;
+
+}  //returns week array
+
+var cutWeekArray = function(busyArray, state){
+
+  console.log("entered function cutWeekArray");
+
+  var weekArray = getWeekArray(state.date, state.time);
+
+  console.log("weekArray", weekArray);
+
+  for(var i=0;i<busyArray.length;i+=2){
+    var x = weekArray.indexOf(busyArray[i]);
+    var y = weekArray.indexOf(busyArray[i+1]);
+    if(x!==-1)weekArray.splice(x,y-x);
+  }
+  console.log("after cutting weekArray", weekArray);
+  return weekArray;
+} // returns week array with available time slots
+
+var limitWeekArray = function(weekArray){
+
+  console.log("entered limitWeekArray");
+
+  var finalArray = [];
+
+  for(var i = 1; i < 8 ; i++){
+    finalArray.push([]);
+  }
+
+  console.log("finalArray", finalArray);
+
+  var j = 0 ;
+
+  for(var i=0;i<weekArray.length; i++){
+    if(finalArray[j].length===3){
+      j++;
+      var date = parseInt(weekArray[i].substring(8,10));
+      var target = date===30 || date===31 ? 1 : date+1;
+      for(var z=0;z<weekArray.length;z++){
+        var look = parseInt(weekArray[z].substring(8,10));
+        if(target === look){
+          i=z;
+          break;
+        }
+      }
+      if(j===7)break;
+    }
+    finalArray[j].push(weekArray[i]);
+  }
+
+  console.log("finalArray", finalArray);
+  var mainArray = [];
+  var k=0;
+  while(mainArray.length!==10){
+    if(finalArray[k].length===0)k++;
+    mainArray.push(finalArray[k].shift());
+  }
+  console.log("mainArray", mainArray);
+  return mainArray;
+} // cuts down weekArray to 10 slots;
+
+var checkConflict = function(user){
+  console.log("entered the funtion checkConflict");
+  return findAttendees(user.pendingState)
+  .then(attendees => {
+      console.log("started forming attendees");
+      var calendarPromises = [];
+      var attendeeCalendars;
+      var busyArray = [];
+
+      attendees.forEach(function(attendee) {
+          var email = encodeURIComponent(attendee.email);
+          var calendarStart = new Date().toISOString();
+          var timeMin = encodeURIComponent(calendarStart);
+          var accessToken = encodeURIComponent(user.googleAccount.access_token);
+          calendarPromises.push(axios.get(`https://www.googleapis.com/calendar/v3/calendars/${email}/events?timeMin=${timeMin}&access_token=${accessToken}`))
+      })
+
+      return Promise.all(calendarPromises)
+      .then(function(calendars) {
+
+          attendeeCalendars = calendars.map(function(calendar) {
+              return calendar.data.items;
+          })
+
+          attendeeCalendars.forEach(function(calendar, index){
+            attendeeCalendars[index] = calendar.filter(function(item){
+              return item.start.dateTime;
+            })
+          })
+
+          attendeeCalendars.forEach(function(calendar, index){
+           attendeeCalendars[index] = calendar.forEach(function(item){
+              var start = item.start.dateTime.split('T');
+              var end = item.end.dateTime.split('T');
+              var startArr = [start[0], start[1].slice(0,5)];
+              var endArr = [end[0], end[1].slice(0,5)];
+              busyArray.push(startArr.join(' '));
+              busyArray.push(endArr.join(' '));
+            })
+          })
+
+          console.log("busyArray", busyArray);
+
+          var checkString = user.pendingState.date + ' ' + user.pendingState.time.substring(0,5);
+
+          console.log("checkString", checkString);
+
+          if(busyArray.indexOf(checkString)===-1)return false; //  no conflict;
+
+          var flag1 = cutWeekArray(busyArray, user.pendingState);
+
+          console.log("after cutting flag1", flag1);
+
+          flag1 = limitWeekArray(flag1);
+
+          console.log("after limiting weeek array", flag1);
+
+          return flag1;
+      })
+      .catch(function(err){
+        console.log(err)
+      });
+  })
+  .then(function(flag1){
+    return flag1;
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+} // creates a busy array using everyones data
+
 
 // TASK FUNCTIONS
 
@@ -93,9 +248,11 @@ var taskFunction = function(data, message, state){
 
 // MEETING FUNCTIONS
 
-var meetingHandler = function({result}, message, state){
+var meetingHandler = function({result}, message, user){ //ccccc
 
   // if all present execute if condition else go to else
+
+  state = user.pendingState;
 
   if(result.parameters.date && result.parameters.time && result.parameters.invitees[0]){
     //set state
@@ -107,15 +264,26 @@ var meetingHandler = function({result}, message, state){
     state.invitees.forEach(function(item){
       inviteString = inviteString + " and " + item;
     })
-    //create im
-    obj.attachments[0].text = `Schedule meeting with ${inviteString} on ${state.date} ${state.time} about ${state.subject}`;
-    web.chat.postMessage(message.channel, "Scheduler Bot", obj,function(err, res) {
-      if (err) {
-        console.log('Error:', err);
-      } else {
-        console.log('Message sent: ', res);
-      }
-    });
+
+    //////
+
+    user.save(function(err, user){
+      var check = true;
+      console.log("enter here after setting pendingState");
+      var flag = checkConflict(user);
+      console.log(flag);
+      if(flag===false)check=false;
+      obj.attachments[0].text = !check ?
+      `Schedule meeting with ${inviteString} on ${state.date} ${state.time} about ${state.subject}`:
+      "CONFLICT BITCH" + flag;
+      web.chat.postMessage(message.channel, "Scheduler Bot", obj,function(err, res) {
+        if (err) {
+          console.log('Error:', err);
+        } else {
+          console.log('Message sent: ', res);
+        }
+      });
+    })
   }
   else {
     //check for all parameters
@@ -129,18 +297,21 @@ var meetingHandler = function({result}, message, state){
       state.time = result.parameters.time;
     }
     if(result.parameters.invitees[0]){
+      state.invitees = result.parameters.invitees;
     }
+    user.save();
     rtm.sendMessage(result.fulfillment.speech, message.channel);
   }
 }
 
-var meetingFunction = function(data, message, state){
+var meetingFunction = function(data, message, user){ //ccccc
+  state = user.pendingState;
   if(!state.date || !state.invitees[0] || !state.time){
-    meetingHandler(data, message, state);
+    meetingHandler(data, message, user); //cccccc
   } else if(state.date && state.time && state.invitees[0]){
     rtm.sendMessage("Reply to previous task status", message.channel);
   } else {
-    meetingHandler(data, message, state);
+    meetingHandler(data, message, user); ///cccccc
   }
 }
 
@@ -155,7 +326,6 @@ var setInvitees = function(myString, state){
   });
   return myArray.join(' ');
 }
-
 
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
@@ -212,8 +382,8 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
           .then(function({ data }){
 
             if(message.text.indexOf("schedule")!==-1){
-              meetingFunction(data, message, user.pendingState);
-              user.save();
+              meetingFunction(data, message, user); //cccccc
+
             }else{
               taskFunction(data, message, user.pendingState);
               user.save();
@@ -307,12 +477,12 @@ app.get('/connect/callback', function(req, res){
 
 })
 
-app.post('/bot-test', function(req,res){
+app.post('/bot-test', function(req,res) {
 
   var data = JSON.parse(req.body.payload);
   console.log("*************reached here******************", data);
 
-    if(data.actions[0].value==="cancel"){
+  if(data.actions[0].value==="cancel"){
       models.User.findOne({slack_ID: JSON.parse(req.body.payload).user.id})
       .then(function(user){
         user.pendingState = {
@@ -329,7 +499,7 @@ app.post('/bot-test', function(req,res){
       res.send("Your request has been cancelled. " + ':pray: :100: :fire:');
     }
 
-    else{
+  else{
       var curTime = Date.now();
       //console.log("*****STATE****", user.pendingState);
       models.User.findOne({slack_ID: JSON.parse(req.body.payload).user.id})
@@ -512,6 +682,7 @@ function calculateStartTimeString(state){
     var startMoment = moment(start);
     return startMoment;
 }
+
 
 function meetingPath(user, state){
 
